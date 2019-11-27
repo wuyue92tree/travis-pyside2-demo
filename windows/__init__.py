330fa0b7776833a94656bbf9dbfcd932a5e3731a
wuyue <wuyue92tree@163.com>
2019-11-27 16:54:16 +0800

    update & make modular

---
+++ windows/__init__.py
@@ -0,0 +1,7 @@
+from PySide2.QtWidgets import QMainWindow
+from windows.ui import mainwindow
+
+class MainWindow(QMainWindow, mainwindow.Ui_MainWindow):
+    def __init__(self, parent=None):
+        super(MainWindow, self).__init__(parent)
+        self.setupUi(self)
